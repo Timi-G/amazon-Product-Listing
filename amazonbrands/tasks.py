b9f06e39c57e699ae5c885ca0f655ab9eef9e768
Timi-G <rotimiolasehinde.a@gmail.com>
2025-04-03 17:35:34 +0100

    Added logger to tasks.py show log info of ongoing tasks

--- amazonbrands/tasks.py
+++ amazonbrands/tasks.py
@@ -1,18 +1,27 @@
 # amazonbrands/tasks.py
+import logging
+
 from celery import shared_task
 from .models import Brand, Product
 from amazonbrands.scraper import scrape_amazon_products
 
+
+logger = logging.getLogger(__name__)
 
 @shared_task
 def scrape_amazon_products_for_all_brands_db():
     """
     Task to scrape Amazon products for each brand in the database.
     """
+    from django.db import close_old_connections
+    close_old_connections()
+
     # Retrieve all brands
     brand_names = Brand.objects.values_list('name', flat=True)
+    logger.info(f"Starting scrape for {len(brand_names)} brands")
 
     for brand_name in brand_names:
+        logger.info(f"Scraping brand: {brand_name}")
         # Call a helper function to scrape products for the specific brand
         scrape_amazon_products(brand_name)
 
